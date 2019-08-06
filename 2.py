0c795ed88c7d82e9b51fe53874b3c2e0860d6c44
moise lucian <moise.lucian@gmail.com>
2019-08-06 19:59:22 +0300

    2

--- 2.py
+++ 2.py
@@ -2,5 +2,4 @@ import functools
 
 a = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]]
 
-print(list(filter(lambda x: x % 2 == 1, map(lambda z: functools.reduce(lambda x, y: x+y, z),a))))
-
+print(list(filter(lambda x: x % 2 == 1, map(lambda z: functools.reduce(lambda x, y: x+y, z), a))))
